7f26513988b921e240b490b8c2207b0a84f4d71a
sillhero <1296928944@qq.com>
2023-04-19 10:42:31 +0800

    account_lowdb_insert
    account_lowdb_insert

--- Account_book/accounts/routes/index.js
+++ Account_book/accounts/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// 导入lowdb
+const low = require('lowdb')
+// 导入FileSync
+const FileSync = require('lowdb/adapters/FileSync')
+// 创建adapter实例
+const adapter = new FileSync(__dirname + '/../data/db.json');
+// 创建lowdb实例
+const db = low(adapter)
+// 导入shortid
+const shortid = require('shortid');
 
 /* GET home page. */
 // 记账本列表
@@ -12,6 +22,11 @@ router.get('/account/create', function(req, res, next) {
 });
 
 router.post('/account', (req, res) => {
+  // 插入数据到数据库中
+  let id = shortid.generate();
+  /// db.get('accounts').push({id:id, ...req.body}).write(); // 尾部插入
+  // 这里的req.body是一个对象，...req.body是将对象展开，之后与id拼接，形成一个新的对象插入数据库中
+  db.get('accounts').unshift({id:id, ...req.body}).write(); // 头部插入
   console.log(req.body);
   res.send('添加记录');
 });
